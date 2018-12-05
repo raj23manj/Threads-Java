f02210a1b6536d20c1cb94465a4476fa2a79d045
rajesh23-drg <rmanjunath@teamdrg.com>
2018-12-05 16:37:02 +0530

    Making Thread to sleep

--- src/com/rajesh/AnotherThread.java
+++ src/com/rajesh/AnotherThread.java
@@ -6,5 +6,14 @@ public class AnotherThread extends Thread{
    @Override
    public void run() {
        System.out.println(ANSI_BLUE +"Hello from " + currentThread().getName());
+
+       try {
+           Thread.sleep(3000);
+           // Exception will be thrown if any other thread interrupts
+       }catch(InterruptedException e) {
+           System.out.println(ANSI_BLUE + "Another Thread Woke Me Up");
+       }
+
+       System.out.println(ANSI_BLUE + "3 seconds passed i'm awake");
    }
 }
